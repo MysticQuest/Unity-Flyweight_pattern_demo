1551bd54f64393e277459205a22f21a181b3f236
Mystic Quest <l1skou@gmail.com>
2019-12-16 01:42:02 +0200

    created landscape by script

--- Assets/CreateCubeMesh.cs
+++ Assets/CreateCubeMesh.cs
@@ -7,12 +7,27 @@ public class CreateCubeMesh : MonoBehaviour
     public int width;
     public int depth;
 
-    enum Cubeside {BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK}; 
+    enum Cubeside { BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK };
+
+
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                Vector3 pos = new Vector3(x,
+                Mathf.PerlinNoise(x * 0.2f, z * 0.2f) * 3,
+                z);
+                CreateCube(x * depth + z, pos);
+            }
+        }
+    }
 
     void CreateQuad(Cubeside side, GameObject parent)
     {
         Mesh mesh = new Mesh();
-        mesh.name = "ScriptedMesh" + side.ToString(); 
+        mesh.name = "ScriptedMesh" + side.ToString();
 
         Vector3[] vertices = new Vector3[4];
         Vector3[] normals = new Vector3[4];
@@ -20,72 +35,72 @@ public class CreateCubeMesh : MonoBehaviour
         int[] triangles = new int[6];
 
         //all possible UVs
-        Vector2 uv00 = new Vector2( 0f, 0f );
-        Vector2 uv10 = new Vector2( 1f, 0f );
-        Vector2 uv01 = new Vector2( 0f, 1f );
-        Vector2 uv11 = new Vector2( 1f, 1f );
+        Vector2 uv00 = new Vector2(0f, 0f);
+        Vector2 uv10 = new Vector2(1f, 0f);
+        Vector2 uv01 = new Vector2(0f, 1f);
+        Vector2 uv11 = new Vector2(1f, 1f);
 
         //all possible vertices 
-        Vector3 p0 = new Vector3( -0.5f,  -0.5f,  0.5f );
-        Vector3 p1 = new Vector3(  0.5f,  -0.5f,  0.5f );
-        Vector3 p2 = new Vector3(  0.5f,  -0.5f, -0.5f );
-        Vector3 p3 = new Vector3( -0.5f,  -0.5f, -0.5f );        
-        Vector3 p4 = new Vector3( -0.5f,   0.5f,  0.5f );
-        Vector3 p5 = new Vector3(  0.5f,   0.5f,  0.5f );
-        Vector3 p6 = new Vector3(  0.5f,   0.5f, -0.5f );
-        Vector3 p7 = new Vector3( -0.5f,   0.5f, -0.5f );
+        Vector3 p0 = new Vector3(-0.5f, -0.5f, 0.5f);
+        Vector3 p1 = new Vector3(0.5f, -0.5f, 0.5f);
+        Vector3 p2 = new Vector3(0.5f, -0.5f, -0.5f);
+        Vector3 p3 = new Vector3(-0.5f, -0.5f, -0.5f);
+        Vector3 p4 = new Vector3(-0.5f, 0.5f, 0.5f);
+        Vector3 p5 = new Vector3(0.5f, 0.5f, 0.5f);
+        Vector3 p6 = new Vector3(0.5f, 0.5f, -0.5f);
+        Vector3 p7 = new Vector3(-0.5f, 0.5f, -0.5f);
 
-        switch(side) //**
+        switch (side) //**
         {
             case Cubeside.BOTTOM:
-                vertices = new Vector3[] {p0, p1, p2, p3};
-                normals = new Vector3[] {Vector3.down, Vector3.down, 
+                vertices = new Vector3[] { p0, p1, p2, p3 };
+                normals = new Vector3[] {Vector3.down, Vector3.down,
                                             Vector3.down, Vector3.down};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] { 3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
             case Cubeside.TOP:
-                vertices = new Vector3[] {p7, p6, p5, p4};
-                normals = new Vector3[] {Vector3.up, Vector3.up, 
+                vertices = new Vector3[] { p7, p6, p5, p4 };
+                normals = new Vector3[] {Vector3.up, Vector3.up,
                                             Vector3.up, Vector3.up};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] {3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
             case Cubeside.LEFT:
-                vertices = new Vector3[] {p7, p4, p0, p3};
-                normals = new Vector3[] {Vector3.left, Vector3.left, 
+                vertices = new Vector3[] { p7, p4, p0, p3 };
+                normals = new Vector3[] {Vector3.left, Vector3.left,
                                             Vector3.left, Vector3.left};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] {3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
             case Cubeside.RIGHT:
-                vertices = new Vector3[] {p5, p6, p2, p1};
-                normals = new Vector3[] {Vector3.right, Vector3.right, 
+                vertices = new Vector3[] { p5, p6, p2, p1 };
+                normals = new Vector3[] {Vector3.right, Vector3.right,
                                             Vector3.right, Vector3.right};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] {3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
             case Cubeside.FRONT:
-                vertices = new Vector3[] {p4, p5, p1, p0};
-                normals = new Vector3[] {Vector3.forward, Vector3.forward, 
+                vertices = new Vector3[] { p4, p5, p1, p0 };
+                normals = new Vector3[] {Vector3.forward, Vector3.forward,
                                             Vector3.forward, Vector3.forward};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] {3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
             case Cubeside.BACK:
-                vertices = new Vector3[] {p6, p7, p3, p2};
-                normals = new Vector3[] {Vector3.back, Vector3.back, 
+                vertices = new Vector3[] { p6, p7, p3, p2 };
+                normals = new Vector3[] {Vector3.back, Vector3.back,
                                             Vector3.back, Vector3.back};
-                uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-                triangles = new int[] {3, 1, 0, 3, 2, 1};
-            break;
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+                break;
         }
 
         mesh.vertices = vertices;
         mesh.normals = normals;
         mesh.uv = uvs;
         mesh.triangles = triangles;
-         
+
         mesh.RecalculateBounds();
 
         GameObject quad = new GameObject("Quad");
